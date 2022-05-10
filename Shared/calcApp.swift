4f862e0e8467f0d27a7fabeb81c557781e420f3a
iosif <iosif@iosif.app>
2022-05-10 13:16:31 +0900

    Initial Commit

---
+++ Shared/calcApp.swift
@@ -0,0 +1,17 @@
+//
+//  calcApp.swift
+//  Shared
+//
+//  Created by iosif on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct calcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
